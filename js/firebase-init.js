1bd766dba628e53ccf97f84033d9769cfb46d967
Adarsh srivastav <143135414+adeegithub1@users.noreply.github.com>
2026-08-19 18:38:48 +0530

    Create firebase-init.js

---
+++ js/firebase-init.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBWKwm1K70O32iTvZwrMeGT1uzBrG9kCQA",
+  authDomain: "sanjay-bag-house.firebaseapp.com",
+  projectId: "sanjay-bag-house",
+  storageBucket: "sanjay-bag-house.firebasestorage.app",
+  messagingSenderId: "700003631201",
+  appId: "1:700003631201:web:743d4b7eaeb1b51f9fea20",
+  measurementId: "G-CSM33N67L2",
+};
